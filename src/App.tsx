91261c6ca070ac500dc320c2cd5b9f79412b8f5d
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2023-02-02 00:28:01 +0400

    Added metamask to preserve connection in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { useWeb3React } from '@web3-react/core';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -11,12 +12,30 @@ const Injected = new InjectedConnector({
 
 function App() {
 
-  const handleConnectClick = () => {
-    activate(Injected);
-  }
-
   const { activate, deactivate } = useWeb3React();
   const { active, chainId, account } = useWeb3React();
+
+  useEffect(() => {
+    const connectWalletOnPageLoad = async () => {
+      if (localStorage?.getItem('isWalletConnected') === 'true') {
+        try {
+          await activate(Injected)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+    connectWalletOnPageLoad()
+  }, []);
+
+  const handleConnectClick = async () => {
+      try {
+        await activate(Injected)
+        localStorage.setItem('isWalletConnected', 'true');
+      } catch (error) {
+        console.log(error)
+      }
+  }
 
   return (
     <BrowserRouter>
