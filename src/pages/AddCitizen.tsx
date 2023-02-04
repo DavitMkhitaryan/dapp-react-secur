d888f4d6377fd2c1f05172e9de86bfea13c24338
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2023-02-05 00:48:04 +0400

    Code refactoring

---
+++ src/pages/AddCitizen.tsx
@@ -0,0 +1,56 @@
+import { useForm } from "react-hook-form";
+interface AddCitizenProps {
+    // to do, type of contract
+    contract: any;
+    account: string | null | undefined;
+}
+
+const AddCitizen: React.FC<AddCitizenProps> = ({ contract, account }) => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            name: '',
+            age: '',
+            city: '',
+            note: ''
+        }
+    });
+
+    return (
+        <main className="h-screen flex items-center justify-center flex-col gap-5">
+            <form onSubmit={handleSubmit((data) => {
+                console.log(data);
+                contract.methods.addCitizen(data.age, data.city, data.name, data.note).send({
+                    from: account
+                }).then(console.log('Success'));
+            })} className='grid grid-cols-3 grid-rows-3 gap-x-5 gap-y-1 border border-gray-200 p-20 shadow-md m-5'>
+                <div className="flex flex-col gap-2">
+                    <label>Name</label>
+                    <input {...register("name", { required: true, maxLength: 150 })} className='border border-gray-200 focus:outline-green-300 rounded-md px-1 py-0.5'/>
+                    {errors.name && <p className="text-red-500">This field is required</p>}
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label>Age</label>
+                    <input {...register("age", { required: true, maxLength: 10 })} className='border border-gray-200 focus:outline-green-300 rounded-md px-1 py-0.5' />
+                    {errors.age && <p className="text-red-500">This field is required</p>}
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label>City</label>
+                    <input {...register("city", { required: true, maxLength: 100 })}  className='border border-gray-200 focus:outline-green-300 rounded-md px-1 py-0.5'/>
+                    {errors.city && <p className="text-red-500">This field is required</p>}
+                </div>
+                <div className="col-span-full flex flex-col gap-2">
+                    <label>Note</label>
+                    <textarea {...register("note", { required: true })} className='border border-gray-200 focus:outline-green-300 rounded-md px-1 py-0.5'></textarea>
+                    {errors.note && <p className="text-red-500">This field is required</p>}
+                </div>
+                <button className="col-span-full text-md bg-gray-300 px-8 py-2 my-4 rounded hover:bg-green-400 active:bg-green-600 max-h-10 flex justify-center items-center" type="submit">
+                    Add
+                </button>
+            </form>
+
+        </main>
+    );
+}
+
+export default AddCitizen;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+interface HomeProps {
+    // to do, type of contract
+    contract: any;
+}
+
+export interface Citizen {
+    id: string;
+    name: string;
+    age: string;
+    city: string;
+}
+
+const Home: React.FC<HomeProps> = ({ contract }) => {
+
+    const [note, setNote] = useState<string>('');
+    const [citizenList, setCitizenList] = useState<Citizen[]>([]);
+
+    useEffect(() => {
+        let newCitizensArr: Citizen[] = [];
+
+        contract.getPastEvents('Citizen', { fromBlock: 0, toBlock: 'latest' }).then((events: any) => {
+            console.log(events);
+            events.forEach((event: any) => {
+                let citizen: Citizen = {
+                    id: event.returnValues[0],
+                    name: event.returnValues[3],
+                    age: event.returnValues[1],
+                    city: event.returnValues[2]
+                }
+                newCitizensArr.push(citizen);
+            });
+            console.log(newCitizensArr);
+            setCitizenList([...newCitizensArr]);
+        });
+    }, []);
+
+    let content = citizenList.map((citizen) => {
+        return (
+            <tr key={citizen.id}>
+                <td>{citizen.id}</td>
+                <td>{citizen.name}</td>
+                <td>{citizen.age}</td>
+                <td>{citizen.city}</td>
+            </tr>
+        );
+    })
+
+    return (
+        <main className="h-screen flex items-center justify-center flex-col gap-5">
+            
+        </main>
+    );
+}
+
+export default Home;
