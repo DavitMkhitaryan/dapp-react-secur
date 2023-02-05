4418223acf4ee30c8dbe69d3bd3edb4effdea98a
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2023-02-05 18:00:21 +0400

    Added table with pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import classnames from 'classnames';
+import { usePagination, DOTS } from '../hooks/usePagination';
+import '../styles/pagination.scss';
+
+interface PaginationProps {
+    onPageChange: any,
+    totalCount: number,
+    siblingCount: number,
+    currentPage: number,
+    pageSize: number,
+    className: any
+}
+
+const Pagination: React.FC<PaginationProps> = props => {
+    const {
+        onPageChange,
+        totalCount,
+        siblingCount = 1,
+        currentPage,
+        pageSize,
+        className
+    } = props;
+
+    const paginationRange = usePagination({
+        currentPage,
+        totalCount,
+        siblingCount,
+        pageSize
+    });
+
+    if (currentPage === 0 || paginationRange.length < 2) {
+        return null;
+    }
+
+    const onNext = () => {
+        onPageChange(currentPage + 1);
+    };
+
+    const onPrevious = () => {
+        onPageChange(currentPage - 1);
+    };
+
+    let lastPage = paginationRange[paginationRange.length - 1];
+
+    return (
+        <tfoot
+            className={classnames('pagination-container border-t-2 pt-2', { [className]: className })}
+        >
+            <tr className='flex flex-row'>
+                <td
+                    className={classnames('pagination-item', {
+                        disabled: currentPage === 1
+                    })}
+                    onClick={onPrevious}
+                >
+                    <div className="arrow left" />
+                </td>
+                {paginationRange.map(pageNumber => {
+                    if (pageNumber === DOTS) {
+                        return <li className="pagination-item dots" key={pageNumber}>&#8230;</li>;
+                    }
+
+                    return (
+                        <td
+                            className={classnames('pagination-item', {
+                                selected: pageNumber === currentPage
+                            })}
+                            onClick={() => onPageChange(pageNumber)}
+                            key={pageNumber}
+                        >
+                            {pageNumber}
+                        </td>
+                    );
+                })}
+                <td
+                    className={classnames('pagination-item', {
+                        disabled: currentPage === lastPage
+                    })}
+                    onClick={onNext}
+                >
+                    <div className="arrow right" />
+                </td>
+            </tr>
+        </tfoot>
+    );
+};
+
+export default Pagination;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
+import Pagination from "../components/Pagination";
 
 interface HomeProps {
     // to do, type of contract
@@ -14,8 +15,10 @@ export interface Citizen {
 
 const Home: React.FC<HomeProps> = ({ contract }) => {
 
-    const [note, setNote] = useState<string>('');
+    // const [note, setNote] = useState<string>('');
     const [citizenList, setCitizenList] = useState<Citizen[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 5;
 
     useEffect(() => {
         let newCitizensArr: Citizen[] = [];
@@ -36,7 +39,13 @@ const Home: React.FC<HomeProps> = ({ contract }) => {
         });
     }, []);
 
-    let content = citizenList.map((citizen) => {
+    const currentTableData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * pageSize;
+        const lastPageIndex = firstPageIndex + pageSize;
+        return citizenList.slice(firstPageIndex, lastPageIndex);
+    }, [currentPage, citizenList]);
+
+    let content = currentTableData.map((citizen) => {
         return (
             <tr key={citizen.id}>
                 <td>{citizen.id}</td>
@@ -45,26 +54,33 @@ const Home: React.FC<HomeProps> = ({ contract }) => {
                 <td>{citizen.city}</td>
             </tr>
         );
-    })
+    });
 
     return (
         <main className="h-screen flex items-center justify-center flex-col gap-5">
-            <div className="flex gap-16">
+            <div className="flex justify-between md:text-lg w-[50rem] px-1">
                 <p>Citizens List</p>
-                <p>Total Records: {citizenList.length}</p>
+                <p>Total Records: <span className="font-bold text-green-500">{citizenList.length}</span></p>
             </div>
-            <table className="border border-gray">
-                <thead>
+            <table className="border border-gray table-fixed w-[50rem] m-12 text-center text-sm md:text-lg">
+                <thead className="border-b-2 shadow-sm">
                     <tr>
-                        <td>ID</td>
-                        <td>NAME</td>
-                        <td>AGE</td>
-                        <td>CITY</td>
+                        <th>ID</th>
+                        <th>NAME</th>
+                        <th>AGE</th>
+                        <th>CITY</th>
                     </tr>
                 </thead>
                 <tbody>
                     {content}
                 </tbody>
+                <Pagination
+                    className="pagination-bar"
+                    currentPage={currentPage}
+                    totalCount={citizenList.length}
+                    pageSize={pageSize}
+                    onPageChange={(page: any) => setCurrentPage(page)}
+                    siblingCount={1} />
             </table>
         </main>
     );
